5284321c1fe08c7681b643d1242be9e8ec2da554
okyun <dhrdbs2980@gmail.com>
2020-04-06 06:51:31 +0900

    board builder sucess TT

--- src/main/java/com/example/demo/dto/BoardDto.java
+++ src/main/java/com/example/demo/dto/BoardDto.java
@@ -1,29 +1,41 @@
 package com.example.demo.dto;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
-@Data
 @Table(name = "board")
-public class BoardDto extends TimeEntity{
+public class BoardDto extends TimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "bno")
     private Integer bno;
 
-    @Column(name = "title")
+    @Column(name = "title",length = 10,nullable = false)
     private String title;
 
-    @Column(name = "content")
+    @Column(name = "content",columnDefinition = "TEXT",nullable = false)
     private String content;
 
-    @Column(name = "writer")
+    @Column(name = "writer",length = 10,nullable = false)
     private String writer;
     //private Date wirteDate;
+
+    @Builder
+    public BoardDto(Integer bno, String title, String content, String writer) {
+        this.bno = bno;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
+
+
 
 
 }
--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -1,4 +1,24 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.BoardDto;
+import com.example.demo.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
 public class BoardService {
+
+   /* @Autowired
+   private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository){
+        this.boardRepository=boardRepository;
+    }
+*/
+
+
+
+
 }
